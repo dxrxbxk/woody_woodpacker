894e157214884fdb4e988fb0fa7c634a8fe84de2
francis <richtofen@debian.backrooms.org>
2024-10-22 12:31:24 +0200

    splited the code, need to move down the segments and sections to inject the shellcode

--- inc/utils.h
+++ inc/utils.h
@@ -1,12 +1,72 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include <bits/stdint-uintn.h>
 #include <stddef.h>
+#include <bits/stdint-uintn.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <sys/mman.h>
+#include <string.h>
 
-int	ft_strlen(char *str);
-int	handle_error(char *msg);
-int handle_syscall(char *msg, int fd);
-void print_data(const uint8_t *data, size_t size);
+#define ELF_32 1
+#define ELF_64 2
+#define SH_PROGBITS 1
+#define SHF_EXECINSTR 4
+#define PT_LOAD 1
+#define PF_X 1
+
+int		ft_strlen(char *str);
+int		handle_error(char *msg);
+int		handle_syscall(char *msg, int fd);
+void	print_data(const uint8_t *data, size_t size);
+
+typedef struct Elf64Hdr_s {
+	uint8_t     e_ident[16];			/* Magic number and other info */
+	uint16_t    e_type;              	/* Object file type */
+	uint16_t    e_machine;           	/* Architecture */
+	uint32_t    e_version;           	/* Object file version */
+	uint64_t    e_entry;             	/* Entry point virtual address */
+	uint64_t    e_phoff;             	/* Program header table file offset */
+	uint64_t    e_shoff;             	/* Section header table file offset */
+	uint32_t    e_flags;             	/* Processor-specific flags */
+	uint16_t    e_ehsize;            	/* ELF header size in bytes */
+	uint16_t    e_phentsize;         	/* Program header table entry size */
+	uint16_t    e_phnum;             	/* Program header table entry count */
+	uint16_t    e_shentsize;         	/* Section header table entry size */
+	uint16_t    e_shnum;             	/* Section header table entry count */
+	uint16_t    e_shstrndx;		   	/* Section header string table index */
+} Elf64Hdr_t;
+
+typedef struct {
+	uint32_t	p_type;				/* Segment type */
+	uint32_t 	p_flags;   			/* Segment flags */
+	uint64_t 	p_offset;  			/* Offset of segment in file */
+	uint64_t 	p_vaddr;   			/* Virtual address of segment */
+	uint64_t 	p_paddr;   			/* Reserved */
+	uint64_t 	p_filesz;  			/* Size of segment in file */
+	uint64_t 	p_memsz;   			/* Size of segment in memory */
+	uint64_t 	p_align;   			/* Alignment of segment */
+} Elf64Phdr_t;
+
+typedef struct {
+	uint32_t	sh_name;				/* Section name (string tbl index) */
+	uint32_t 	sh_type;       		/* Section type */
+	uint64_t 	sh_flags;      		/* Section flags */
+	uint64_t 	sh_addr;       		/* Address where section is to be loaded */
+	uint64_t 	sh_offset;     		/* File offset of section data */
+	uint64_t 	sh_size;       		/* Size of section data */
+	uint32_t 	sh_link;       		/* Section index linked to this section */
+	uint32_t 	sh_info;       		/* Extra information */
+	uint64_t 	sh_addralign;  		/* Section alignment */
+	uint64_t 	sh_entsize;			/* Entry size if section holds table */
+} Elf64Shdr_t;
+
+typedef struct data_s {
+	uint8_t					*_file_map;
+	size_t					_file_size;
+} data_t;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,119 +1,173 @@
-#include <bits/stdint-uintn.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <stdint.h>
-#include <sys/mman.h>
-#include <string.h>
 #include "utils.h"
 
-#define ELF_32 1
-#define ELF_64 2
-#define SH_PROGBITS 1
-#define SHF_EXECINSTR 4
-#define PT_LOAD 1
-#define PF_X 1
 
+const char	shellcode[]			= "\x48\x31\xc0\xb0\x01\xbf\x01\x00\x00\x00\x48\x8d\x35\x13\x00\x00\x00\xba\x0f\x00\x00\x00\x0f\x05\x48\x31\xc0\xb0\x3c\xbf\x2a\x00\x00\x00\x0f\x05\x2e\x2e\x2e\x2e\x57\x4f\x4f\x44\x59\x2e\x2e\x2e\x2e\x0a\x0d";
+size_t		shellcode_size		= sizeof(shellcode) - 1;
 
-typedef struct Elf64Hdr_s {
-  uint8_t     e_ident[16];			/* Magic number and other info */
-  uint16_t    e_type;              	/* Object file type */
-  uint16_t    e_machine;           	/* Architecture */
-  uint32_t    e_version;           	/* Object file version */
-  uint64_t    e_entry;             	/* Entry point virtual address */
-  uint64_t    e_phoff;             	/* Program header table file offset */
-  uint64_t    e_shoff;             	/* Section header table file offset */
-  uint32_t    e_flags;             	/* Processor-specific flags */
-  uint16_t    e_ehsize;            	/* ELF header size in bytes */
-  uint16_t    e_phentsize;         	/* Program header table entry size */
-  uint16_t    e_phnum;             	/* Program header table entry count */
-  uint16_t    e_shentsize;         	/* Section header table entry size */
-  uint16_t    e_shnum;             	/* Section header table entry count */
-  uint16_t    e_shstrndx;		   	/* Section header string table index */
-} Elf64Hdr_t;
+data_t*	data_getter(void) {
+	static data_t *data = NULL;
 
+	if (data == NULL) {
+		data = malloc(sizeof(data_t));
+		if (data == NULL)
+			handle_error("malloc");
 
-typedef struct {
-	uint32_t p_type;				/* Segment type */
-	uint32_t p_flags;   			/* Segment flags */
-	uint64_t p_offset;  			/* Offset of segment in file */
-	uint64_t p_vaddr;   			/* Virtual address of segment */
-	uint64_t p_paddr;   			/* Reserved */
-	uint64_t p_filesz;  			/* Size of segment in file */
-	uint64_t p_memsz;   			/* Size of segment in memory */
-	uint64_t p_align;   			/* Alignment of segment */
-} Elf64_Phdr;
+		data->_file_map = NULL;
+		data->_file_size = 0;
 
-typedef struct {
-	uint32_t sh_name;				/* Section name (string tbl index) */
-	uint32_t sh_type;       		/* Section type */
-	uint64_t sh_flags;      		/* Section flags */
-	uint64_t sh_addr;       		/* Address where section is to be loaded */
-	uint64_t sh_offset;     		/* File offset of section data */
-	uint64_t sh_size;       		/* Size of section data */
-	uint32_t sh_link;       		/* Section index linked to this section */
-	uint32_t sh_info;       		/* Extra information */
-	uint64_t sh_addralign;  		/* Section alignment */
-	uint64_t sh_entsize;			/* Entry size if section holds table */
-} Elf64_Shdr;
-
-
-const char shellcode[] = "\x48\x31\xc0\xb0\x01\xbf\x01\x00\x00\x00\x48\x8d\x35\x13\x00\x00\x00\xba\x0f\x00\x00\x00\x0f\x05\x48\x31\xc0\xb0\x3c\xbf\x2a\x00\x00\x00\x0f\x05\x2e\x2e\x2e\x2e\x57\x4f\x4f\x44\x59\x2e\x2e\x2e\x2e\x0a\x0d";
-
-
-size_t shellcode_length = sizeof(shellcode) - 1;
-
-
-void *get_section_data(void *file_map, Elf64Hdr_t *header, const char *section_name, size_t *section_size) {
-    Elf64_Shdr *sections = (Elf64_Shdr *)((char *)file_map + header->e_shoff);
-    Elf64_Shdr *section;
-    Elf64_Shdr *strtab = &sections[header->e_shstrndx];
-    char *strtab_data = (char *)file_map + strtab->sh_offset;
-
-    for (size_t i = 0; i < header->e_shnum; i++) {
-        section = &sections[i];
-        const char *name = &strtab_data[section->sh_name];
-
-        if (strcmp(name, section_name) == 0) {
-            *section_size = section->sh_size;
-            return (char *)file_map + section->sh_offset;
-        }
-    }
-
-    return NULL;
-}
-
-void patch_shellcode(void *file_map, Elf64Hdr_t *header, Elf64_Phdr *load_segment, void *section_data, size_t section_size) {
-    void *shellcode_addr = (char *)file_map + load_segment->p_vaddr;
-    size_t segment_size = load_segment->p_memsz;
-	size_t pfile = load_segment->p_filesz;
-
-	printf("shellcode_addr: %p\n", shellcode_addr);
-	printf("segment_size: %lu\n", segment_size);
-	printf("section_size: %lu\n", section_size);
-	print_data((uint8_t *)shellcode_addr, segment_size);
-
-	section_size += shellcode_length;
-	segment_size += shellcode_length;
-	pfile += shellcode_length;
-
-
-	if (section_size > segment_size) {
-		handle_error("Shellcode too big\n");
 	}
 
-
-	memmove((uint8_t *)shellcode_addr + shellcode_length, shellcode_addr, 
-	memcpy(shellcode_addr, shellcode, shellcode_length);
-
-
-	print_data((uint8_t *)shellcode_addr, segment_size);
-
-	header->e_entry = load_segment->p_vaddr;
+	return data;
 }
 
-int	check_file(char *filename) {
+
+void	data_destroyer(void) {
+	data_t *data = data_getter();
+
+	if (data) {
+		if (data->_file_map)
+			munmap(data->_file_map, data->_file_size);
+		free(data);
+	}
+}
+
+
+ //		Map in the umodified target ELF executable and the user-supplied payload, a position-independent blob
+ //		Locate the first executable segment by parsing program headers
+ //		Locate the last section in the executable segment by parsing section headers
+ //		Expand the last section (in the segment) section header's sh_size and program header's p_memsz/p_filesz by the size of the user-supplied payload
+ //		Fixup section headers' sh_offset's and program headers' p_offset's (move down sections and segments to make room for the payload and a small "stager stub")
+ //		Fix offsets in the ELF header (e_shoff, e_phoff, etc..)
+ //		Modify the ELF header e_entry (ELF entrypoint offset) to point to the injected code
+ //		Create a new ELF containing the injected code and modified ELF headers
+
+static Elf64Phdr_t*	parse_program_headers(data_t *data) {
+	
+	Elf64Hdr_t	*header = (Elf64Hdr_t *)data->_file_map;
+	Elf64Phdr_t	*program_headers = (Elf64Phdr_t *)(data->_file_map + header->e_phoff);
+
+	for (size_t i = 0; i < header->e_phnum; i++) {
+		if (program_headers[i].p_flags & PF_X) {
+			printf("Found executable segment at offset 0x%lx, size %li\n", program_headers[i].p_offset, program_headers[i].p_filesz);
+			return &program_headers[i];
+		}
+	}
+
+	return NULL;
+}
+
+// make space for the shellcode in the last section of the executable segment
+//
+void	expand_section(data_t *data, Elf64Shdr_t *shdr, Elf64Phdr_t *phdr)
+
+{
+	Elf64Hdr_t	*header = (Elf64Hdr_t *)data->_file_map;
+	printf("--------------------\n");
+	printf("header->e_shoff: %lx\n", header->e_shoff);
+	printf("header->e_phoff: %lx\n", header->e_phoff);
+	printf("shdr->sh_offset: %lx\n", shdr->sh_offset);
+	printf("shdr->sh_size: %li\n", shdr->sh_size);
+	printf("phdr->p_offset: %lx\n", phdr->p_offset);
+	printf("phdr->p_filesz: %li\n", phdr->p_filesz);
+	printf("phdr->p_memsz: %li\n", phdr->p_memsz);
+	printf("header->e_entry: %lx\n", header->e_entry);
+	
+
+//	// expand the section
+//	shdr->sh_size += shellcode_size;
+//
+//	// expand the segment
+//	phdr->p_filesz += shellcode_size;
+//	phdr->p_memsz += shellcode_size;
+//
+//	// move down sections
+//	Elf64Shdr_t	*section_headers = (Elf64Shdr_t *)(data->_file_map + header->e_shoff);
+//	for (size_t i = 0; i < header->e_shnum; i++) {
+//			section_headers[i].sh_offset += shellcode_size;
+//	}
+//
+//	// move down segments
+//	Elf64Phdr_t	*program_headers = (Elf64Phdr_t *)(data->_file_map + header->e_phoff);
+//	for (size_t i = 0; i < header->e_phnum; i++) {
+//			program_headers[i].p_offset += shellcode_size;
+//	}
+//
+////	 fix offsets in the ELF header
+//	header->e_shoff += shellcode_size;
+//	header->e_phoff += shellcode_size;
+//
+//
+	// encrypt the shellcode
+	//uint8_t key = gen_key();
+	//encrypt((uint8_t *)shellcode, shellcode_size, key);
+
+	// append the shellcode to the section
+	memcpy(data->_file_map + shdr->sh_offset + shdr->sh_size - shellcode_size, shellcode, shellcode_size);
+
+	header->e_entry = shdr->sh_addr + shdr->sh_size - shellcode_size;
+
+	//printf("Shellcode key: 0x%02x\n", key);
+	printf("Shellcode size: %li\n", shellcode_size);
+	printf("Shellcode appended at offset 0x%lx\n", shdr->sh_offset + shdr->sh_size - shellcode_size);
+
+}
+
+static Elf64Shdr_t	*parse_section_headers(data_t *data, Elf64Phdr_t *phdr) {
+
+	uint64_t	segment_end			= phdr->p_offset + phdr->p_filesz;
+	Elf64Hdr_t	*header				= (Elf64Hdr_t *)data->_file_map;
+	Elf64Shdr_t	*section_headers	= (Elf64Shdr_t *)(data->_file_map + header->e_shoff);
+
+	for (size_t i = 0; i < header->e_shnum; i++) {
+		if (section_headers[i].sh_offset + section_headers[i].sh_size == segment_end) {
+			printf("Found last section in segment at offset 0x%lx, size %li\n", section_headers[i].sh_offset, section_headers[i].sh_size);
+			printf("Section name: %s\n", data->_file_map + section_headers[header->e_shstrndx].sh_offset + section_headers[i].sh_name);
+			return section_headers + i;
+		}
+	}
+	return NULL;
+}
+
+#define MAP_ANONYMOUS 0x20
+
+static int patch_new_file(data_t *data, Elf64Phdr_t *phdr, Elf64Shdr_t *shdr) {
+
+	int fd = open("woody", O_CREAT | O_WRONLY | O_TRUNC, 0755);
+	if (fd == -1)
+		handle_syscall("open", fd);
+
+	if (write(fd, data->_file_map, data->_file_size) == -1)
+		handle_syscall("write", fd);
+
+	return (EXIT_SUCCESS);
+}
+
+static int	inject_shellcode(void) {
+
+	data_t *data = data_getter();
+
+
+	Elf64Phdr_t	*phdr = parse_program_headers(data);
+	if (phdr == NULL)
+		return handle_error("No executable segment found\n");
+
+	Elf64Shdr_t	*shdr = parse_section_headers(data, phdr);
+	if (shdr == NULL)
+		return handle_error("No section found\n");
+
+	if ((shdr + 1)->sh_addr - shdr->sh_addr - shdr->sh_size >= shellcode_size) {
+		printf("Shellcode fits in section\n");
+		expand_section(data, shdr, phdr);
+		patch_new_file(data, phdr, shdr);
+	} else {
+		return handle_error("Shellcode too big for section\n");
+	}
+
+	//check if the shellcode fits in the section
+	return (EXIT_SUCCESS);
+}
+
+static int	check_file(char *filename) {
 
 	int	fd = open(filename, O_RDONLY);
 	if (fd == -1)
@@ -138,50 +192,19 @@ int	check_file(char *filename) {
 	else if (header->e_ident[4] != ELF_64)
 		return handle_error("Unknown ELF format\n");
 
+	data_t *data		= data_getter();
+	data->_file_map		= file_map;
+	data->_file_size	= file_size;
 
-	Elf64_Phdr *phdr = (Elf64_Phdr *)((char *)file_map + header->e_phoff);
-	Elf64_Phdr *load_segment = NULL;
-	for (size_t i = 0; i < header->e_phnum; i++) {
-		if (phdr[i].p_type == PT_LOAD && (phdr[i].p_flags & PF_X)) {
-			load_segment = &phdr[i];
-			break;
-		}
-	}
-
-	if (load_segment == NULL)
-		return handle_error("No executable segment found\n");
-
-	size_t section_size;
-	void *section_data = get_section_data(file_map, header, ".text", &section_size);
-	if (section_data == NULL)
-		return handle_error("No .text section found\n");
-
-	patch_shellcode(file_map, header, load_segment, section_data, section_size);
-
-	printf("load_segment->p_vaddr: %lx\n", load_segment->p_vaddr);
-	printf("header->e_entry: %lx\n", header->e_entry);
-
-	header->e_entry = load_segment->p_vaddr;
-
-	int fd2 = open("woody", O_CREAT | O_RDWR | O_TRUNC, 0755);
-	if (fd2 == -1)
-		handle_syscall("open", fd2);
-
-	write(fd2, file_map, file_size);
-
-	if (munmap(file_map, file_size) == -1)
-		handle_syscall("munmap", fd);
-
-	close(fd);
-	close(fd2);
-
-	return 0;
+	return inject_shellcode();
 }
 
 int main(int argc, char *argv[]) {
   if (argc == 2) {
-	  if (check_file(argv[1]))
+	  if (check_file(argv[1])) {
+		  data_destroyer();
 		  return (EXIT_FAILURE);
+	  }
 	  return (EXIT_SUCCESS);
   } else
 	  return handle_error("Usage: ./woody_woodpacker <filename>\n");
